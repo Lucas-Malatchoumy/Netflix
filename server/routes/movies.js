e155de38fd17a8cd6c3084b6a9705740e43124df
Lucas Malatchoumy <malatchoumylucas.com>
2022-05-08 23:39:45 +0200

    feat(movies):create movies routes

---
+++ server/routes/movies.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const {
+    getLastMovies,
+    getAdventureMovies,
+    getActionMovies,
+    getFantasticMovies,
+    getHorrorMovies,
+    getSciFiMovies,
+    getComedyMovies,
+    getDramaMovies,
+    getMovie,
+    getMovies,
+    getGenres,
+    getMovieActor
+} = require('../controllers/movies')
+
+
+router.get('/getlastMovies', getLastMovies);
+router.get('/genres/:genre', getMovies)
+router.get('/genres', getGenres)
+router.get('/adventureMovies', getAdventureMovies);
+router.get('actionMovies', getActionMovies);
+router.get('fantasticMovies', getFantasticMovies);
+router.get('horrorMovies', getHorrorMovies);
+router.get('sci-fiMovies', getSciFiMovies);
+router.get('comedyMovies', getComedyMovies);
+router.get('dramaMovies', getDramaMovies)
+router.get('/:movie', getMovie);
+router.get('/mostViewedFilms');
+router.get('/searchByGenre');
+router.get('/searchByTitle');
+router.get('/:movie/actors', getMovieActor)
+
+module.exports = router;
