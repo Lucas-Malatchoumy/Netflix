a2a524fc470a2e8ab52f08c66fe3cfede9ba06f7
Lucas Malatchoumy <malatchoumylucas.com>
2022-05-08 23:32:19 +0200

    feat(lastmovies):create last movies component

---
+++ front/src/components/privateRoute.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Navigate, Outlet} from 'react-router-dom'
+import Navbar from "./navbar";
+
+const useAuth = () =>{
+  const token = localStorage.getItem('token');
+  if (token) {
+    return true
+  } 
+  else {
+    return false
+  }
+}
+
+const ProtectedRoutes = () => {
+  const auth=useAuth()
+  return auth?<><Navbar /><Outlet /></>: <Navigate to="/home"/>
+}
+
+export default ProtectedRoutes;
