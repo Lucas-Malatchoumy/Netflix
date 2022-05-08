d50aaf1b7017417c54a2910d70282db643e0f7a1
Lucas Malatchoumy <malatchoumylucas.com>
2022-05-08 23:41:04 +0200

    build(server):create server

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const db = require("./test");
+const moviesRoutes = require("./routes/movies")
+const userRoutes = require("./routes/user")
+app.use(cors());
+app.use(express.json())
+
+app.use('/Netflix/movies', moviesRoutes)
+app.use('/Netflix/user', userRoutes);
+
+app.listen('3001', () => {
+  console.log("ca roule");
+})
