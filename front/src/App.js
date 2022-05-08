5c88e45bec300b6d9b23cdd8e1bb83fdd6bb24b6
Lucas Malatchoumy <malatchoumylucas.com>
2022-05-08 23:23:37 +0200

    fix(front):change name

---
+++ front/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import Register from './pages/register';
+import Home from './pages/home';
+import { Routes, Route } from "react-router-dom"
+import Movie from './components/movie'
+import './App.css';
+import Dashboard from './pages/dashboard';
+import Profile from './pages/profile';
+import Login from './pages/login';
+import ProtectedRoutes from './components/privateRoute';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<ProtectedRoutes/>}>
+            <Route path='/movies/:movieId' element={<Movie />} ></Route>
+            <Route path='/dashboard' element={<Dashboard />}></Route>
+            <Route path='/profile' element={<Profile />}></Route>
+        </Route>
+        <Route path='/home' element={<Home />}></Route>
+        <Route path='/register' element={<Register />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
